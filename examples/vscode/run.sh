217b8c6ea63dc19ddf476d0e4d7edeeda0110821
Mark Veltzer <mark.veltzer@gmail.com>
2025-12-06 20:32:16 +0200

    no commit message given

--- examples/vscode/run.sh
+++ examples/vscode/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -eu
 docker run -it \
-	-e WAYLAND_DISPLAY=$WAYLAND_DISPLAY \
-	-e XDG_RUNTIME_DIR=/tmp \
-	-v $XDG_RUNTIME_DIR/$WAYLAND_DISPLAY:/tmp/$WAYLAND_DISPLAY \
-	-v $HOME/projects:/home/dev/projects \
-	--device /dev/dri \
+	-e WAYLAND_DISPLAY="${WAYLAND_DISPLAY}" \
+	-e XDG_RUNTIME_DIR="/tmp" \
+	-v "${XDG_RUNTIME_DIR}/${WAYLAND_DISPLAY}:/tmp/${WAYLAND_DISPLAY}" \
+	-v "${HOME}/projects:/home/dev/projects" \
+	--device "/dev/dri" \
 	vscode
